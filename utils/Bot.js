f3ad084a30291f7dd2b48cdfc7b235d14672c201
Micah Riggan <micahriggan@gmail.com>
2017-02-08 00:12:42 -0600

    fixed tick and ontick

--- utils/Bot.js
+++ utils/Bot.js
@@ -6,6 +6,16 @@ class Bot {
     this.tickCommands = [];
     this.middleware = Ware();
     this.client = null;
+
+
+		this.listen = this.listen.bind(this);
+		this.say = this.say.bind(this);
+		this.start = this.start.bind(this);
+		this.onStart = this.onStart.bind(this);
+		this.onTick = this.onTick.bind(this);
+		this.tick = this.tick.bind(this);
+		this.use = this.use.bind(this);
+		this.run = this.run.bind(this);
   }
   listen(client, triggerName) {
     this.client = client;
@@ -29,8 +39,7 @@ class Bot {
   tick() {
     console.log("-- Update Tick --");
     this.run(this.tickCommands);
-    let commandCycle = Settings.getSetting('coreApp', 'app_cycle');
-    setTimeout(this.tick, commandCycle * 1000);
+    setTimeout(this.tick, 1000);
   }
   //tail recursively handle lists of functions
   use(commands) {
